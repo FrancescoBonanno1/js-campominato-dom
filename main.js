6c425771cf18c0d63604fc1d016b06aadaa3c831
Francesco Bonanno <francescosoapbonanno@gmail.com>
2023-11-08 17:08:12 +0100

    versione attuale

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //andiamo a recuperare classi e formattazioni con create element e classlist add, generiamo un ciclo for per fare il lavoro sporco al posto nostro
 let contenitore = document.getElementById("contenitore");
-const livello = document.getElementsByClassName("livello").value;
+let numerocelle = 80;
 
 //ora voglio impostare le difficoltà e legarle ad un ciclo o ad una funzione che mi crei le celle desiderate
 
