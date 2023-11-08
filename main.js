67d5d805986d140ee140a0ce8499614fa9c68cdb
Francesco Bonanno <francescosoapbonanno@gmail.com>
2023-11-08 17:44:15 +0100

    versione con value accettato e logica ricorretta

--- main.js
+++ main.js
@@ -4,14 +4,21 @@ let numerocelle = 80;
 
 //ora voglio impostare le difficoltà e legarle ad un ciclo o ad una funzione che mi crei le celle desiderate
 
-document.getElementById("btnGO").addEventListener("click", function(){
-    for(let i = 1; i<= numerocelle; i++){
-    let cella = document.createElement("div");
-    contenitore.appendChild(cella);
-    cella.classList.add("formattazione");
-    cella.innerText = (i)
+document.getElementById("btnGO").addEventListener("click", function () {
+    const difficoltà = document.getElementById("livello").value;
+    console.log(difficoltà);
+
+    
+    for (let i = 1; i <= numerocelle; i++) {
+        let cella = document.createElement("div");
+        contenitore.appendChild(cella);
+        cella.classList.add("formattazione");
+        cella.innerText = (i)
 
 
-    cella.addEventListener("click", function() {
-        cella.classList.toggle("colorecelle")
-    console.log("hai cliccato la cella :" + i)});}})
+        cella.addEventListener("click", function () {
+            cella.classList.toggle("colorecelle")
+            console.log("hai cliccato la cella :" + i)
+        });
+    }
+})
